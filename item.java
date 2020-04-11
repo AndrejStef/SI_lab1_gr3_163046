3cb88153e76909a458b2387c2de175fbf2059300
AndrejStef <astefanoski93@gmail.com>
2020-04-11 20:50:43 +0200

    taxReturn method finished

--- item.java
+++ item.java
@@ -2,7 +2,20 @@ package prv;
 import java.io.*;
 
 public class Item {
-	public static void main(String[] args) {
+	
 	double DDV;
+	public Item(double ddv) {
+		DDV = ddv;
 	}
+	public void setDDV(double ddv) {
+		this.DDV = ddv;
+	}
+	public double getDDV() {
+		return DDV;
+	}
+	double getTaxReturn() {
+			return DDV * 0.15;		
+	}
+	
+	
 }
